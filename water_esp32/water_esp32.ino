e7525e29a065476847a341ef1494cdd083550f33
pan <xmlshop88@gmail.com>
2019-07-17 15:58:25 -0700

    ok

---
+++ water_esp32/water_esp32.ino
@@ -0,0 +1,96 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+#define TIMEOUT 200 
+
+String mqttid_sim7020 = "0";
+String mqttsvip = "94.191.14.111";
+String mqttsvport = "2000";
+
+********wifi********************
+const char* ssid = "";
+const char* password = "";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+
+  Serial.begin(115200);
+  delay(500);
+
+  Serial1.begin(115200,SERIAL_8N1, 4, 2); 
+  Serial2.begin(115200,SERIAL_8N1, 16, 17);
+  
+  delay(1000);
+  // put your setup code here, to run once:
+
+send_at("AT+CMQNEW=?",2000);
+delay(2000);
+ 
+//send_at("AT+CMQNEW=\"" +  mqttsvip   +   "\", \"" +  mqttsvport  + "\" ,5000,1024",5000);
+//delay(5000);
+
+delay(4000);
+mqttid_sim7020 =  getmqttid();
+Serial.println("aha mqttid is  " + mqttid_sim7020 );
+
+send_at("AT+CMQCON=" +   mqttid_sim7020   +  ",3,\"myclient\",600,0,0",3000);
+delay(3000);
+
+send_at("AT+CMQSUB=" + mqttid_sim7020  + ",\"hello\",0",3000);
+delay(3000);
+
+//send_at("AT+CMQPUB=" + mqttid_sim7020  + ",\"hello\",1,0,0,4,\"wind\"",3000);
+//delay(3000);
+}
+
+void loop() {
+
+  send_at("AT+CMQPUB=" + mqttid_sim7020  + ",\"hello\",1,0,0,4,\"1234\"",3000);
+  
+  delay(6000);
+  // put your main code here, to run repeatedly:
+
+}
+
+
+String getmqttid()
+{  
+  
+ String res = ""; 
+Serial2.println("AT+CMQNEW=\"" +  mqttsvip   +   "\", \"" +  mqttsvport  + "\" ,5000,1024");
+long int time = millis(); 
+while(time + 2000 > millis())
+{  
+while(Serial2.available())
+{
+   res+=  Serial2.readString();;
+}
+}
+ Serial.print(res); 
+
+int leng = res.length();
+  Serial.println("aha  find-" + res.substring(leng-4,leng-2) + "-"  +  "  -" +   res.substring(leng-9,leng-8)   + "-");
+  
+ return res.substring(leng-9,leng-8);
+}
+
+
+void send_at(String command, int timeout)
+{  
+ 
+Serial2.println(command);
+long int time = millis(); 
+while(time + timeout > millis())
+{  
+while(Serial2.available())
+{
+  String c = Serial2.readString();
+  Serial.print(c); 
+}
+}
+}
